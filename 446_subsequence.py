ec390304b643466c6ac119f705a1b4b0d411bce0
Kirill Chirkunov, Ph.D <kirill.chirkunov@gmail.com>
2024-06-21 09:01:57 +0400

    Update 446_subsequence.py
    Renamed vars for clear understanding, added comments

--- 446_subsequence.py
+++ 446_subsequence.py
@@ -1,20 +1,23 @@
 from typing import List
 from collections import defaultdict
+
 class Solution:
     def numberOfArithmeticSlices(self, nums: List[int]) -> int:
         n = len(nums)
-        cnt = 0
-        mem = defaultdict(lambda:defaultdict(int))
+        total_count = 0
+        # Use a defaultdict of defaultdicts to store the count of arithmetic slices ending at each index with a given difference
+        mem = defaultdict(lambda: defaultdict(int))
+
         for i in range(n):
             for j in range(i):
                 diff = nums[i] - nums[j]
-                pcnt = mem[j][diff]
-                mem[i][diff] = mem[i][diff] + pcnt + 1
-                cnt += pcnt
-        return cnt
+                previous_count = mem[j][diff]
+                mem[i][diff] += previous_count + 1
+                total_count += previous_count
 
+        return total_count
+
+# Example usage
 sol = Solution()
-
-print(sol.numberOfArithmeticSlices([1,2,1,2,4,1,5,10]))
-
-print(sol.numberOfArithmeticSlices([2,4,6,8,10]))
+print(sol.numberOfArithmeticSlices([1, 2, 1, 2, 4, 1, 5, 10]))  # Example 1
+print(sol.numberOfArithmeticSlices([2, 4, 6, 8, 10]))  # Example 2
