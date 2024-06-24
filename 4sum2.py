6fbb05405620c919a3946400a240ecd91986a0b3
Kirill Chirkunov, Ph.D <kirill.chirkunov@gmail.com>
2024-06-24 14:18:06 +0400

    Update 4sum2.py
    Refactoring and documentation

--- 4sum2.py
+++ 4sum2.py
@@ -1,33 +1,34 @@
 from typing import List
 from collections import Counter
+
 class Solution:
     def fourSumCount(self, nums1: List[int], nums2: List[int], nums3: List[int], nums4: List[int]) -> int:
+        """
+        Counts the number of tuples (i, j, k, l) such that:
+        nums1[i] + nums2[j] + nums3[k] + nums4[l] == 0
+        
+        Args:
+        nums1: List[int] - The first list of integers.
+        nums2: List[int] - The second list of integers.
+        nums3: List[int] - The third list of integers.
+        nums4: List[int] - The fourth list of integers.
+        
+        Returns:
+        int - The count of tuples (i, j, k, l) that satisfy the condition.
+        """
+        
+        # Count the sums of all pairs from nums1 and nums2
+        count_ab = Counter(a + b for a in nums1 for b in nums2)
+        
+        # Count the sums of all pairs from nums3 and nums4 and check against count_ab
+        count = sum(count_ab[-c - d] for c in nums3 for d in nums4)
+        
+        return count
 
-        nums = [nums1, nums2, nums3, nums4]
-        nl = len(nums)
-        numsd = [Counter(nums[i]) for i in range(nl)]
-        nnumsd = []
-        ld = {}
-        for key0 in numsd[0]:
-            for key1 in numsd[1]:
-                key = key0 + key1
-                ld[key] = ld.get(key, 0) + numsd[0][key0]*numsd[1][key1]
-        nnumsd.append(ld)
-        rd = {}
-        for key0 in numsd[-1]:
-            for key1 in numsd[-2]:
-                key = key0 + key1
-                rd[key] = rd.get(key, 0) + numsd[-1][key0]*numsd[-2][key1]
-        nnumsd.append(rd)
-        d = {}
-        for key0 in nnumsd[0]:
-            for key1 in nnumsd[1]:
-                key = key0 + key1
-                d[key] = d.get(key, 0) + nnumsd[0][key0]*nnumsd[1][key1]
-        return d.get(0, 0)
-
+# Example usage:
 s = Solution()
-print(s.fourSumCount(nums1 = [1,2], nums2 = [-2,-1], nums3 = [-1,2], nums4 = [0,2]))
+result = s.fourSumCount(nums1=[1, 2], nums2=[-2, -1], nums3=[-1, 2], nums4=[0, 2])
+print(result)  # Output: 2
 
 
 
