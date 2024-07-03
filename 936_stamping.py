239d8bde2d89d20fccd3af1a0e9b36ac2be1e16b
Kirill Chirkunov, Ph.D <kirill.chirkunov@gmail.com>
2024-07-03 16:34:24 +0400

    Code refactoring, added documentation for 936 problem solution

--- 936_stamping.py
+++ 936_stamping.py
@@ -1,24 +1,50 @@
 from typing import List
-from functools import cache
-import sys
 
 class Solution:
     def movesToStamp(self, stamp: str, target: str) -> List[int]:
+        """
+        Find the sequence of moves to stamp the target with the given stamp.
+
+        Args:
+        stamp (str): The stamp string.
+        target (str): The target string.
+
+        Returns:
+        List[int]: The list of indices where the stamp is applied to transform the target.
+        """
+        
         def hamming_distance(arr1, arr2):
+            """
+            Calculate the hamming distance between two arrays.
+            
+            Args:
+            arr1 (List[str]): The first array.
+            arr2 (List[str]): The second array.
+            
+            Returns:
+            int: The hamming distance.
+            """
             return sum(c1 == c2 for c1, c2 in zip(arr1, arr2))
 
-        global buffer
         buffer = list('?' * len(target))
         n = len(target)
         m = len(stamp)
         stamp = list(stamp)
         target = list(target)
-        global hops
         hops = 0
         log = []
 
         def min_stamps(first):
-            global hops
+            """
+            Recursively determine the minimum number of stamps needed.
+            
+            Args:
+            first (bool): Flag indicating if it's the first call.
+            
+            Returns:
+            bool: True if the stamping is successful, False otherwise.
+            """
+            nonlocal hops
             dst = hamming_distance(buffer, target)
             if dst == n:
                 hops -= 1
@@ -26,7 +52,7 @@ class Solution:
             if (dst == 0 and not first) or hops >= 10:
                 hops -= 1
                 return False
-            for i in range(n-m + 1):
+            for i in range(n - m + 1):
                 old_state = buffer[i:(i + m)]
                 if old_state == stamp:
                     continue
@@ -41,8 +67,10 @@ class Solution:
         min_stamps(True)
         return log
 
+# Example usage
 sol = Solution()
-print(sol.movesToStamp(stamp = "abc", target = "ababc"))
+print(sol.movesToStamp(stamp="abc", target="ababc"))
+
 
 
 
