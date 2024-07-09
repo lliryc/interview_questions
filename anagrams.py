d3d5067e5bb27839c0597226d30f5f164be0358a
Kirill Chirkunov, Ph.D <kirill.chirkunov@gmail.com>
2024-07-09 21:35:30 +0400

    Refactoring+comments (anagrams solution)

--- anagrams.py
+++ anagrams.py
@@ -1,41 +1,52 @@
-#!/bin/python3
-
-import math
-import os
-import random
-import re
-import sys
 import itertools
 
-# Complete the sherlockAndAnagrams function below.
 def sherlockAndAnagrams(s):
-    fulllst = sorted([sorted(s[i:j]) for i in range(0, len(s)) for j in range(i+1,len(s)+1)])
-    print(fulllst)
-    stat = list(filter(lambda i : i > 1,[len(list(v)) for (k,v) in itertools.groupby(fulllst)]))
-    sm = sum([(i*(i-1))//2 for i in stat])
-    return sm
+    """
+    Find the number of anagrammatic pairs of substrings in the given string.
 
+    Parameters:
+    s (str): The input string.
+
+    Returns:
+    int: The number of anagrammatic pairs of substrings.
+    """
+    # Generate a list of sorted substrings
+    substrings = sorted([sorted(s[i:j]) for i in range(len(s)) for j in range(i + 1, len(s) + 1)])
+    print(substrings)
+    
+    # Group the sorted substrings and count occurrences
+    counts = list(filter(lambda x: x > 1, [len(list(group)) for key, group in itertools.groupby(substrings)]))
+    
+    # Calculate the number of anagrammatic pairs using the combination formula
+    anagram_pairs = sum((count * (count - 1)) // 2 for count in counts)
+    
+    return anagram_pairs
 
 def sherlockAnagrams2(s):
+    """
+    Alternative implementation to find the number of anagrammatic pairs of substrings in the given string.
+
+    Parameters:
+    s (str): The input string.
+
+    Returns:
+    int: The number of anagrammatic pairs of substrings.
+    """
     l = len(s)
-    substr_lst = sorted([sorted(s[i:j] ) for i in range(l) for j in range(i+1, l+1)])
-    grouped_lst = [len(list(group)) for (k,group) in itertools.groupby(substr_lst)]
-    grouped_list = filter(lambda k: k > 1, grouped_lst)
-    return sum([i*(i-1)//2 for i in grouped_list])
-
-
+    
+    # Generate a list of sorted substrings
+    substrings = sorted([sorted(s[i:j]) for i in range(l) for j in range(i + 1, l + 1)])
+    
+    # Group the sorted substrings and count occurrences
+    grouped_counts = [len(list(group)) for key, group in itertools.groupby(substrings)]
+    
+    # Filter out groups with only one occurrence
+    filtered_counts = filter(lambda count: count > 1, grouped_counts)
+    
+    # Calculate the number of anagrammatic pairs using the combination formula
+    anagram_pairs = sum((count * (count - 1)) // 2 for count in filtered_counts)
+    
+    return anagram_pairs
 
 if __name__ == '__main__':
     print(sherlockAnagrams2("ifailuhkqq"))
-    # fptr = open(os.environ['OUTPUT_PATH'], 'w')
-    #
-    # q = int(input())
-    #
-    # for q_itr in range(q):
-    #     s = input()
-    #
-    #     result = sherlockAndAnagrams(s)
-    #
-    #     fptr.write(str(result) + '\n')
-    #
-    # fptr.close()
