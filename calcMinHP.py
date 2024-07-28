01489bc78012f56121992ab6afc0f02a5b6fc872
Kirill Chirkunov, Ph.D. <kirill.chirkunov@gmail.com>
2024-07-28 15:27:28 +0400

    Added solution for calculate min hp

---
+++ calcMinHP.py
@@ -0,0 +1,15 @@
+
+class Solution:
+    def calculateMinimumHP(self, dungeon):
+        m, n = len(dungeon), len(dungeon[0])
+        dp = [[float("inf")] * (n + 1) for _ in range(m + 1)]
+        dp[m - 1][n], dp[m][n - 1] = 1, 1
+
+        for i in range(m - 1, -1, -1):
+            for j in range(n - 1, -1, -1):
+                dp[i][j] = max(min(dp[i + 1][j], dp[i][j + 1]) - dungeon[i][j], 1)
+
+        return dp[0][0]
+
+sol = Solution()
+print(sol.calculateMinimumHP(dungeon = [[-2,-3,3],[-5,-10,1],[10,30,-5]]))
